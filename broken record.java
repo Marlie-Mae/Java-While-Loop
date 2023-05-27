4677413b9a2c53f515985ddbd863defd08cb1bac
Marlie Mae Awayan <128160451+Marlie-Mae@users.noreply.github.com>
2023-05-27 11:17:06 +0800

    Add files via upload
    Looping strings works like playing a broken record – it repeats something over and over again. But what's great about loops is that we can define as to how many times it will repeat something with proper code, and that's what we're about to do now using while() loops.
    
    Instructions:
    Input a positive integer.
    Using while() loop, repeatedly print out the message "CodeChum is awesome" for the same number of times as that of the inputted integer. Each outputted message must be separated in new lines.
    Don't forget to make an increment/decrement variable that will increase its value per iteration so as to not encounter a forever loop and have errors, okay?

---
+++ broken record.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String args[]) {
+        
+		Scanner input = new Scanner(System.in);
+        int i = 1;
+		int n = input.nextInt();
+
+		while(i <= n) {
+            System.out.println("CodeChum is awesome");
+            i++;
+            
+		}
+
+	}
+}
